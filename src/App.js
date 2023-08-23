acec24b501e33660a8972d9d685362df7e685aaf
DuDeXsUraj <surajgusain2004@gmail.com>
2023-08-23 23:24:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   return (
-  //  <ProductsPage></ProductsPage>
   <BrowserRouter>
     <h1 className='title' >GenMedia Assignment</h1>
     <div className='underline'></div>
